7cdeea3bc106e026691b0d1df4541cd3fd8dcfb8
Henry Megwai <henrymegwai@gmail.com>
2023-11-03 07:34:22 +0100

    fix: updated Swagger doc title for pen api

--- Presentation.Api/Program.cs
+++ Presentation.Api/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options => { 
-   options.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather Forcast Service", Version = "v1" });
+   options.SwaggerDoc("v1", new OpenApiInfo { Title = "Weather Forecast Service", Version = "v1" });
 });
 
 builder.Services.AddInfrastructureService(builder.Configuration);
